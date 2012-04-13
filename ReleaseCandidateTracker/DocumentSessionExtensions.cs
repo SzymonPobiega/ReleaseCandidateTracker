551fa42309daa93b660a95b0b98fb4a9cc45da74
Szymon Pobiega <szymon.pobiega@gmail.com>
2012-04-13 06:40:17 +0200

    Merged extension classes

--- ReleaseCandidateTracker/DocumentSessionExtensions.cs
+++ ReleaseCandidateTracker/DocumentSessionExtensions.cs
@@ -1,10 +1,33 @@
+using System.IO;
+using System.Web.Mvc;
 using Raven.Client;
+using Raven.Json.Linq;
 using ReleaseCandidateTracker.Models;
 
 namespace ReleaseCandidateTracker
 {
     public static class DocumentSessionExtensions
     {
+        public static void PutAttachment(this IDocumentSession documentSession, string key, Stream fileContents)
+        {
+            var metadata = new RavenJObject();
+            documentSession.Advanced.DatabaseCommands.PutAttachment(key, null, fileContents, metadata);
+        }
+
+        public static ActionResult GetAttachmentResult(this IDocumentSession documentSession, string key, string contentType)
+        {
+            var attachment = documentSession.Advanced.DatabaseCommands.GetAttachment(key);
+            if (attachment != null)
+            {
+                var result = new FileStreamResult(attachment.Data(), contentType)
+                {
+                    FileDownloadName = key
+                };
+                return result;
+            }
+            return null;
+        }
+
         public static ReleaseCandidate GetCandidate(this IDocumentSession documentSession, string versionNumber)
         {
             var result = documentSession.Load<ReleaseCandidate>(versionNumber.MakeCandidateId());
